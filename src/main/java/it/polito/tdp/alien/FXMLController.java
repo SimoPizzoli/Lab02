4d22d99a420fe312207c0424be90e0218f8c7743
Simone <Simone@172.21.173.18>
2022-03-15 15:29:15 +0100

    Esercizio 1

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -5,20 +5,74 @@
 package it.polito.tdp.alien;
 
 import java.net.URL;
+import java.util.*;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
+    
+    @FXML
+    private Label txtErrore;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtTraduzione;
+    
+    private Map <String,String> dizionario = new TreeMap<String,String>();
+
+    @FXML
+    void handleClear(ActionEvent event) {
+    	txtParola.clear();
+    	txtTraduzione.clear();
+    	txtErrore.setText("");
+    }
+
+    @FXML
+    void handleTraslate(ActionEvent event) {
+    	String parola = txtParola.getText();
+    	parola.toLowerCase();
+    	String c[] = parola.split(" ");
+    	if(c.length == 2) {
+    		if(c[0].matches("[a-zA-Z]*") && c[1].matches("[a-zA-Z]*")) {
+   				dizionario.put(c[0], c[1]);
+   				txtTraduzione.setText("Parola correttamente salvata nel dizionario.\n");
+   				txtErrore.setText("");
+    		}
+    		else 
+    			txtErrore.setText("Errore. Caratteri NON ammessi");
+    	}
+    	else {
+    		if(parola.matches("[a-zA-Z]*")) {
+    			if(dizionario.containsKey(parola)) {
+    				txtTraduzione.setText(dizionario.get(parola));
+    				txtErrore.setText("");
+    			}
+    			else
+    				txtErrore.setText("Errore. Inserire nella forma <parola aliena>_<traduzione>");
+    		}
+    		else
+    			txtErrore.setText("Errore. Caratteri NON ammessi");
+    	}
+    	txtParola.clear();
+    }  		
+
+    @FXML
     void initialize() {
-
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTraduzione != null : "fx:id=\"txtTraduzione\" was not injected: check your FXML file 'Scene.fxml'.";
+        
     }
 
 }
